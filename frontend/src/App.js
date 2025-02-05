411bef63d5ab651791d0c4522839292b67009594
htxbby313 <167072930+htxbby313@users.noreply.github.com>
2025-02-04 22:55:46 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div>
       <h1>🎵 DJ Streaming App</h1>
       <video controls>
-        <source src="http://localhost:8000/stream" type="video/mp4" />
+        <source src="https://ns1.idgt.me:10000/stream" type="video/mp4" />
       </video>
     </div>
   );
